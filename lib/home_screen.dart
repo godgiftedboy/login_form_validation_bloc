174059f8d68a37621fecd8456d9d2de58580da88
godgiftedboy <61019893+godgiftedboy@users.noreply.github.com>
2024-01-04 15:52:12 +0545

    using BlocBuilder but later we use Provider syntax as in the video which renders whole scaffold once the state changes

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:login_form_validation/bloc/auth_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,7 +8,16 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // body: ,
-        );
+      body: Center(
+        child: BlocBuilder<AuthBloc, AuthState>(
+          builder: (context, state) {
+            if (state is AuthSuccess) {
+              return Text(state.uid);
+            }
+            return const Text("Validate yourself properly");
+          },
+        ),
+      ),
+    );
   }
 }
